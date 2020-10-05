e87340be09b3563ebd977addb77b0bf10085ef6f
ajg7 <ajgebara@gmail.com>
2020-10-05 15:50:40 -0500

    success Read operations

--- data/seeds/accountsRouter.js
+++ data/seeds/accountsRouter.js
@@ -2,6 +2,34 @@ const express = require("express");
 const db = require("../../data/dbConfig");
 const router = express.Router();
 
+//Get
 
+router.get("/", (request, response) => {
+    db.select("*").from("accounts")
+        .then(accounts => {
+            response.status(200).json({data: accounts})
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).json({error: error.message})
+        })
+})
+
+router.get("/:id", (request, response) => {
+    db("accounts").where("id", "=", request.params.id)
+        .then(accounts => {
+            response.status(200).json({data: accounts});
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(500).json({error: error.message});
+        })
+})
+
+//Post
+
+//Put
+
+//Delete
 
 module.exports = router;
