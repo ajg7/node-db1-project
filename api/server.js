f183df7c9388e6df5d03675cf016d0d188f68d0a
ajg7 <ajgebara@gmail.com>
2020-10-05 15:40:05 -0500

    server is up and running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const accountsRouter = require("../data/seeds/accountsRouter");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountsRouter);
+
+server.get("/", (request, response) => {
+    response.status(200).json({Frankenstein: "It's alive"})
+})
 
 module.exports = server;
